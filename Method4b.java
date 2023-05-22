206aea903ae5d2eeeaaac9bc3dcac0386f335433
ruifukuda <rufk222@gmail.com>
2023-05-22 11:49:17 +0900

    Method4bにコメント追加

--- Method4b.java
+++ Method4b.java
@@ -8,6 +8,12 @@ public class Method4b{
 		System.out.println("税込み価格は" + calcPrice(price, taxRatio) + "です");
 
 	}
+
+	/*calcPriceメソッド
+	 * 税抜き価格と、消費税率から税込み価格を求める
+	 * 引数:price(int)税抜き価格,taxRatio(int)税率
+	 * 戻り値:税込み価格(int)
+	 */
 	public static int calcPrice(int price, int taxRatio){
 		price = (int)(price * (1 + taxRatio/100.0)); 
 		return price;
